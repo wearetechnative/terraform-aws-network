8b681aeb6eb5b40920d4a904731eb277d66c5401
Pim Snel <post@pimsnel.com>
2023-10-30 17:49:02 +0100

    add and SSM remote control endpoints

--- endpoint_gateway.tf
+++ endpoint_gateway.tf
@@ -15,3 +15,61 @@ resource "aws_vpc_endpoint" "dynamodb" {
 
   route_table_ids = [ for k, v in aws_route_table.this : v.id ]
 }
+
+
+locals {
+  endpoints = {
+    "endpoint-ssm" = {
+      name = "ssm"
+    },
+    "endpoint-ssm-messages" = {
+      name = "ssmmessages"
+    },
+    "endpoint-ec2-messages" = {
+      name = "ec2messages"
+    }
+  }
+}
+
+
+# Security group for VPC Endpoints
+resource "aws_security_group" "vpc_endpoint_security_group" {
+  name_prefix = "vpc-endpoint-sg"
+  vpc_id      = aws_vpc.this.id
+  description = "security group for VPC Endpoints"
+
+  # Allow inbound HTTPS traffic
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.this.cidr_block]
+    description = "Allow HTTPS traffic from VPC"
+  }
+
+
+  # Allow inbound HTTPS traffic
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.this.cidr_block]
+    description = "Allow HTTPS traffic from VPC"
+  }
+
+  tags = {
+    Name = "VPC Endpoint security group"
+  }
+}
+
+
+resource "aws_vpc_endpoint" "endpoints" {
+  vpc_id            = aws_vpc.this.id
+  for_each          = local.endpoints
+  vpc_endpoint_type = "Interface"
+  service_name      = "com.amazonaws.${data.aws_region.current.name}.${each.value.name}"
+  # Add a security group to the VPC endpoint
+  security_group_ids = [aws_security_group.vpc_endpoint_security_group.id]
+}
+
+
