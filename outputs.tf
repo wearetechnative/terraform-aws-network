da417284c6f8667f6d7ceef4b235d8862ee22811
Wouter van der Toorren <githubwto@toorren.net>
2024-03-04 14:06:33 +0100

    Revert "add cidr_block to outputs"
    This reverts commit 822228ad339eaeaf47a4323217bdc7b986b0a057.

--- outputs.tf
+++ outputs.tf
@@ -10,11 +10,3 @@ output "subnet_groups" {
 output "vpc_id" {
   value = aws_vpc.this.id
 }
-
-output "cidr_blocks" {
-  value = aws_vpc.this.cidr_block
-}
-
-output "subnet_ids" {
-  value = [aws_vpc.this.subnet_ids]
-}
