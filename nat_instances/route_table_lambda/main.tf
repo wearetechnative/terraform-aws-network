a2115163af8a332e3cc054b7c80f0b20d99929fb
andrew@technative.eu <andrew@technative.eu>
2024-11-13 11:05:21 +0100

    updated lambda ref

--- nat_instances/route_table_lambda/main.tf
+++ nat_instances/route_table_lambda/main.tf
@@ -1,7 +1,7 @@
 # stolen from https://github.com/hashicorp/terraform/issues/8344
 
 module "lambda" {
-  source = "git@github.com:wearetechnative/terraform-aws-lambda?ref=6d454d4a19f565c1ef423f870b28cadf6e4800d6"
+  source = "git@github.com:wearetechnative/terraform-aws-lambda?ref=ae0530a86c1eff7460d638e5ef885908ff5b8f88"
 
   name              = var.name
   role_arn          = module.iam_role.role_arn
